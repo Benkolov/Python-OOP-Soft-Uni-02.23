e5fbc7189256ece8d93030249efdd9274995f214
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-03-07 22:57:54 +0200

    Initial commit

--- 2.Exercise/04.Encapsulation - Exercise/Restaurant/project/beverage/beverage.py
+++ 2.Exercise/04.Encapsulation - Exercise/Restaurant/project/beverage/beverage.py
@@ -2,7 +2,7 @@ from project.product import Product
 
 
 class Beverage(Product):
-    def __init__(self,name, price, milliliters):
+    def __init__(self, name: str, price: float, milliliters: float):
         super().__init__(name, price)
         self.__milliliters = milliliters
 
--- 2.Exercise/04.Encapsulation - Exercise/Restaurant/project/beverage/coffee.py
+++ 2.Exercise/04.Encapsulation - Exercise/Restaurant/project/beverage/coffee.py
@@ -2,8 +2,8 @@ from project.beverage.hot_beverage import HotBeverage
 
 
 class Coffee(HotBeverage):
-    MILLILITERS = 50
-    PRICE = 3.50
+    MILLILITERS: int = 50
+    PRICE: float = 3.50
 
     def __init__(self, name: str, caffeine: int):
         super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
