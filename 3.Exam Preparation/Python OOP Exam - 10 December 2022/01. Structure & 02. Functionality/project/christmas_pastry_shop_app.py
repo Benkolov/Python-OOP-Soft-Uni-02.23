81ca772ab79f628c289de5df0de043fd9f0e7263
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-03-31 09:42:04 +0300

    Initial commit

---
+++ 3.Exam Preparation/Python OOP Exam - 10 December 2022/01. Structure & 02. Functionality/project/christmas_pastry_shop_app.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from project.booths.booth import Booth
+from project.delicacies.delicacy import Delicacy
+from project.delicacies.gingerbread import Gingerbread
+from project.delicacies.stolen import Stolen
+
+
+class ChristmasPastryShopApp:
+    VALID_DELICACIES = {
+        "Gingerbread": Gingerbread,
+        "Stolen": Stolen,
+    }
+
+    def __init__(self):
+        self.booths: List[Booth] = []
+        self.delicacies: List[Delicacy] = []
+        self.income = 0
+
+    def add_delicacy(self, type_delicacy: str, name: str, price: float):
+        delicacy = [d for d in self.delicacies if d.name == name]
+
+        if delicacy:
+            raise Exception(f"{name} already exists!")
+
+        if type_delicacy not in self.VALID_DELICACIES:
+            raise Exception(f"{type_delicacy} is not on our delicacy menu!")
+
+        delicacy = self.VALID_DELICACIES[type_delicacy](name, price)
+        self.delicacies.append(delicacy)
+        return f"Added delicacy {name} - {type_delicacy} to the pastry shop."
+
+    def add_booth(self, type_booth: str, booth_number: int, capacity: int):
+        pass
+
+    def reserve_booth(self, number_of_people: int):
+        pass
+
+    def order_delicacy(self, booth_number: int, delicacy_name: str):
+        pass
+
+    def leave_booth(self, booth_number: int):
+        pass
+
+    def get_income(self):
+        return f"Income: {self.income:.2f}lv."
