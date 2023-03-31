c8bffdec3afc18af7579b3eb606cb83386c23976
Boyan <bbenkolov@gmail.com>
2023-03-31 19:48:26 +0300

    Initial commit

--- 3.Exam Preparation/Python OOP Exam - 10 December 2022/03. Unit Testing/project/test/test_toy_store.py
+++ 3.Exam Preparation/Python OOP Exam - 10 December 2022/03. Unit Testing/project/test/test_toy_store.py
@@ -1,1 +1,30 @@
 from project.toy_store import ToyStore
+from unittest import TestCase, main
+
+
+class TestToyStore(TestCase):
+    def setUp(self):
+        self.store = ToyStore()
+
+    def test_correct__init(self):
+        for key in range(ord("A"), ord("G") + 1):
+            self.assertIsNone(self.store.toy_shelf[chr(key)])
+
+        self.assertEqual(7, len(self.store.toy_shelf))
+
+    def test_add_toy_on_non_existing_shelf_raises_exception(self):
+        pass
+
+    def test_add_toy_that_is_already_on_shelf_raises_exception(self):
+        pass
+
+    def test_add_toy_to_a_full_shelf_raises_exception(self):
+        pass
+
+    def test_add_toy_successfully_returns_string(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
