b7c4a143fea87566d8683ecca624992cc0dc2592
Boyan <bbenkolov@gmail.com>
2023-04-02 19:19:04 +0300

    Initial commit

--- 3.Exam Preparation/Python OOP Exam - 10 December 2022/03. Unit Testing/project/test/test_toy_store.py
+++ 3.Exam Preparation/Python OOP Exam - 10 December 2022/03. Unit Testing/project/test/test_toy_store.py
@@ -13,16 +13,52 @@ class TestToyStore(TestCase):
         self.assertEqual(7, len(self.store.toy_shelf))
 
     def test_add_toy_on_non_existing_shelf_raises_exception(self):
-        pass
+        with self.assertRaises(Exception) as ex:
+            self.store.add_toy("Z", "HotWheels")
+
+        self.assertEqual("Shelf doesn't exist!", str(ex.exception))
 
     def test_add_toy_that_is_already_on_shelf_raises_exception(self):
-        pass
+        self.store.add_toy("A", "HotWheels")
+
+        with self.assertRaises(Exception) as ex:
+            self.store.add_toy("A", "HotWheels")
+
+        self.assertEqual("Toy is already in shelf!", str(ex.exception))
 
     def test_add_toy_to_a_full_shelf_raises_exception(self):
-        pass
+        self.store.add_toy("A", "Doll")
+
+        with self.assertRaises(Exception) as ex:
+            self.store.add_toy("A", "HotWheels")
+
+        self.assertEqual("Shelf is already taken!", str(ex.exception))
 
     def test_add_toy_successfully_returns_string(self):
-        pass
+        result = self.store.add_toy("A", "HotWheels")
+
+        self.assertEqual("Toy:HotWheels placed successfully!", result)
+        self.assertEqual("HotWheels", self.store.toy_shelf["A"])
+
+    def test_remove_toy_from_non_existing_shelf_raises_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.store.remove_toy("Z", "HotWheels")
+
+        self.assertEqual("Shelf doesn't exist!", str(ex.exception))
+
+    def test_remove_non_existing_toy_from_shelf_raises_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.store.remove_toy("A", "HotWheels")
+
+        self.assertEqual("Toy in that shelf doesn't exists!", str(ex.exception))
+
+    def test_remove_toy_correct(self):
+        self.store.toy_shelf["A"] = "HotWheels"
+
+        result = self.store.remove_toy("A", "HotWheels")
+
+        self.assertIsNone(self.store.toy_shelf["A"])
+        self.assertEqual("Remove toy:HotWheels successfully!", result)
 
 
 if __name__ == "__main__":
